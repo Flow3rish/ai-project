ada0651c58065449f5c4daed2a5a319d2edcbf66
Vít Chrubasík <flow3rish@protonmail.com>
2021-06-06 16:45:03 +0200

    Initial commit

---
+++ src/test/java/cz/chrubasik/aiproject/AiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.chrubasik.aiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
