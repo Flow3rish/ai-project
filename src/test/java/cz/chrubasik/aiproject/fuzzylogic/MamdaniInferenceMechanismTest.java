7944cf0eedcb87ba6c408943a74b20dd15133c16
Vít Chrubasík <flow3rish@protonmail.com>
2021-06-11 02:22:50 +0200

    test(mamdani): correct test case

--- src/test/java/cz/chrubasik/aiproject/fuzzylogic/MamdaniInferenceMechanismTest.java
+++ src/test/java/cz/chrubasik/aiproject/fuzzylogic/MamdaniInferenceMechanismTest.java
@@ -14,7 +14,6 @@ class MamdaniInferenceMechanismTest {
 
 	@Test
 	void test() {
-		MamdaniInferenceMechanism mamdaniInferenceMechanism = new MamdaniInferenceMechanism();
 		
 		HashMap<String, FuzzySetRealsLinearContinuous> m_x_1 = new HashMap<>();
 		m_x_1.put("ledova_voda", FuzzySetRealsLinearContinuous.ofLeftCorner(0D, 4D, 7D));
@@ -23,7 +22,6 @@ class MamdaniInferenceMechanismTest {
 		m_x_1.put("tepla_voda", FuzzySetRealsLinearContinuous.ofSymmetricTrapezoidalFuzzyNumber(30D, 37D, 2D));
 		m_x_1.put("horka_voda", FuzzySetRealsLinearContinuous.ofRightCorner(38D, 42D, 100D));
 		FuzzyLinguisticVariable teplota_vody = new FuzzyLinguisticVariable("teplota_vody", m_x_1);
-		mamdaniInferenceMechanism.addLinguisticVariable(teplota_vody);
 		
 		
 		HashMap<String, FuzzySetRealsLinearContinuous> m_x_2 = new HashMap<>();
@@ -31,43 +29,44 @@ class MamdaniInferenceMechanismTest {
 		m_x_2.put("indiferentni_zakaznik", FuzzySetRealsLinearContinuous.ofSymmetricTrapezoidalFuzzyNumber(9D, 15D, 2D));
 		m_x_2.put("spokojeny_zakaznik", FuzzySetRealsLinearContinuous.ofSymmetricTrapezoidalFuzzyNumber(18D, 26D, 2D));
 		FuzzyLinguisticVariable spokojenost_zakaznika = new FuzzyLinguisticVariable("spokojenost_zakaznika", m_x_2);
-		mamdaniInferenceMechanism.addLinguisticVariable(spokojenost_zakaznika);
 		
 		Rule rule1 = Rule.builder()
 				.antecedent("teplota_vody is studena_voda", "teplota_vody is ledova_voda")
 				.operatorType(OperatorType.OR)
 				.consequent("spokojenost_zakaznika is nespokojeny_zakaznik")
 				.build();
-		mamdaniInferenceMechanism.addRule(rule1);
 		
 		Rule rule2 = Rule.builder()
 				.antecedent("teplota_vody is vlazna_voda")
 				.operatorType(OperatorType.OR)
 				.consequent("spokojenost_zakaznika is indiferentni_zakaznik")
 				.build();
-		mamdaniInferenceMechanism.addRule(rule2);
 		
 		Rule rule3 = Rule.builder()
 				.antecedent("teplota_vody is tepla_voda")
 				.operatorType(OperatorType.OR)
 				.consequent("spokojenost_zakaznika is spokojeny_zakaznik")
 				.build();
-		mamdaniInferenceMechanism.addRule(rule3);
 		
 		Rule rule4 = Rule.builder()
 				.antecedent("teplota_vody is horka_voda")
 				.operatorType(OperatorType.OR)
 				.consequent("spokojenost_zakaznika is nespokojeny_zakaznik")
 				.build();
-		mamdaniInferenceMechanism.addRule(rule4);
 		
 	
-		HashMap<String, Double> measurements = new HashMap<>();
-		measurements.put("teplota_vody", 20D);
-		mamdaniInferenceMechanism.setMeasurements(measurements);
-
-		System.out.println(mamdaniInferenceMechanism.runInference().get("spokojenost_zakaznika"));
-		System.out.println(mamdaniInferenceMechanism.defuzzifyCOA().get("spokojenost_zakaznika"));
+		MamdaniInferenceMechanism mamdaniInferenceMechanism = new MamdaniInferenceMechanism();
+		mamdaniInferenceMechanism
+				.addLinguisticVariable(teplota_vody)
+				.addLinguisticVariable(spokojenost_zakaznika)
+				.addRule(rule1)
+				.addRule(rule2)
+				.addRule(rule3)
+				.addRule(rule4)
+				.addMeasurement("teplota_vody", 20D);
+		
+		mamdaniInferenceMechanism.runInference();
+		assertEquals(12D, mamdaniInferenceMechanism.defuzzifyCOA().get("spokojenost_zakaznika").getElement());
 		
 		
 		
